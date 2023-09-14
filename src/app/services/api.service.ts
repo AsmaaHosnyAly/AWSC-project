52da9400de151dab72f32a4f6728084ee2f0e338
unknown <R.badry@ISDT.LOCAL>
2023-09-14 17:05:43 +0300

    strAdd restructure & solve issues

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1744,6 +1744,9 @@ export class ApiService {
       data
     );
   }
+  getStrAddDetailsByAddId(id:any) {
+    return this.http.get<any>(`${this.url}/STRAdd/GeTAddDetailsByAddId/${id}`);
+  }
   putStrAddDetails(data: any) {
     console.log('strOpenDetails data: ', data);
     return this.http.put<any>(
