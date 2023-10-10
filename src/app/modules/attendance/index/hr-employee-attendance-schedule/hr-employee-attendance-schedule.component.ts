7a02ee75a30a07a662798e7ca04b31939be4249e
Feeby <Feeby@ISDT.local>
2023-10-10 11:49:41 +0300

    kk

--- src/app/modules/attendance/index/hr-employee-attendance-schedule/hr-employee-attendance-schedule.component.ts
+++ src/app/modules/attendance/index/hr-employee-attendance-schedule/hr-employee-attendance-schedule.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-hr-employee-attendance-schedule',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./hr-employee-attendance-schedule.component.css']
 })
 export class HrEmployeeAttendanceScheduleComponent {
-
+  
 }
+
+
