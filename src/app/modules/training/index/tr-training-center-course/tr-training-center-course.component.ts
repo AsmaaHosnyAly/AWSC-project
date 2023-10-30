acfb53ed5c5f05b351872a2d53345f1617f6fa86
Asmaa Hossni <A.Hossni@ISDT.local>
2023-10-30 10:48:49 +0200

    add page tilte in training screens

--- src/app/modules/training/index/tr-training-center-course/tr-training-center-course.component.ts
+++ src/app/modules/training/index/tr-training-center-course/tr-training-center-course.component.ts
@@ -66,7 +66,7 @@ export class TrTrainingCenterCourseComponent {
       startWith(''),
       map((value) => this._filterTraingingCenteres(value))
     );
-    global.getPermissionUserRoles(4, 'stores', ' الإدارة العامة للتدريب', '')
+    global.getPermissionUserRoles('IT', '', 'الإدارة العامة للتدريب', '')
   }
   ngOnInit(): void {
     // console.log(productForm)
