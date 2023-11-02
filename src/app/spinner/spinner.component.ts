a2e6503328d614904fa4021c59a13b8a49e8f313
unknown <F.Ragab@ISDT.LOCAL>
2023-11-02 10:35:18 +0200

    edit spinner

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { NgxSpinnerService } from "ngx-spinner";
 @Component({
   selector: 'app-spinner',
   templateUrl: './spinner.component.html',
