8587f0fffbbb8ef2aa90e15105ee83fd2ad234ea
Asmaa Hossni <A.Hossni@ISDT.local>
2023-11-02 09:27:30 +0200

    add page tilte in training screens

---
+++ src/app/modules/training/index/tr-budget-dialog/tr-budget-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tr-budget-dialog',
+  templateUrl: './tr-budget-dialog.component.html',
+  styleUrls: ['./tr-budget-dialog.component.css']
+})
+export class TrBudgetDialogComponent {
+
+}
---
+++ src/app/modules/training/index/tr-budget/tr-budget.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tr-budget',
+  templateUrl: './tr-budget.component.html',
+  styleUrls: ['./tr-budget.component.css']
+})
+export class TrBudgetComponent {
+
+}
