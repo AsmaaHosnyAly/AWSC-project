2a77c2c37bcaf1b5b79acf7d20799273bc465e00
Asmaa Hossni <A.Hossni@ISDT.local>
2023-11-05 10:18:33 +0200

    add page tilte in training screens

--- src/app/modules/training/index/tr-budget-dialog/tr-budget-dialog.component.ts
+++ src/app/modules/training/index/tr-budget-dialog/tr-budget-dialog.component.ts
@@ -1,4 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators, FormControl } from '@angular/forms';
+import { ApiService } from '../../services/api.service';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { map, startWith } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatOptionSelectionChange } from '@angular/material/core';
+import { HotkeysService } from 'angular2-hotkeys';
+import { Hotkey } from 'angular2-hotkeys';
+export class course {
+  constructor(public id: number, public name: string) {}
+}
+export class TrainingCenter {
+  constructor(public id: number, public name: string) {}
+}
 
 @Component({
   selector: 'app-tr-budget-dialog',
@@ -6,5 +27,234 @@ import { Component } from '@angular/core';
   styleUrls: ['./tr-budget-dialog.component.css']
 })
 export class TrBudgetDialogComponent {
-
+  transactionUserId=localStorage.getItem('transactionUserId')
+  courseCtrl: FormControl;
+  filteredCourseStates: Observable<course[]>;
+  cityStates: course[] = [];
+  selectedCityState: course | undefined;
+  trainingCenterCtrl: FormControl;
+  filteredTrainingCenteres: Observable<TrainingCenter[]>;
+  trainingCenteres:TrainingCenter[] = [];
+  selectedTrainingCenter: TrainingCenter | undefined;
+  formcontrol = new FormControl('');  
+  modelForm !:FormGroup;
+  actionBtn : string = "حفظ"
+  selectedOption:any;
+  getModelData: any;
+  Id:string  | undefined | null;
+   vendorDt:any={
+  id:0,
 }
+commname:any;
+dataSource!: MatTableDataSource<any>;
+existingNames: string[] = [];
+@ViewChild(MatPaginator) paginator!: MatPaginator;
+@ViewChild(MatSort) sort!: MatSort;
+@ViewChild(MatAccordion)
+accordion!: MatAccordion;
+cityStatelist:any;
+cityStateName: any;
+trainingCenterList: any;
+trainingCenterName: any;
+  constructor(private formBuilder : FormBuilder,
+    private api : ApiService,
+    private hotkeysService: HotkeysService,
+    private readonly route:ActivatedRoute,
+    @Inject(MAT_DIALOG_DATA) public editData : any,
+    private dialogRef : MatDialogRef<TrBudgetDialogComponent>,
+    private toastr: ToastrService) {
+      this.courseCtrl = new FormControl();
+      this.filteredCourseStates = this.courseCtrl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filterCityStates(value))
+      );
+      this.trainingCenterCtrl = new FormControl();
+      this.filteredTrainingCenteres = this.trainingCenterCtrl.valueChanges.pipe(
+        startWith(''),
+        map(value => this._filterTrainingCenteres(value))
+      );
+    }
+    ngOnInit(): void {
+      this.getExistingNames(); // Fetch existing names
+      this.modelForm = this.formBuilder.group({
+        //define the components of the form
+     
+      
+      code : ['',Validators.required],
+      name : ['',Validators.required],
+      address : ['',Validators.required],
+      type : ['',Validators.required],
+      capacity : ['',Validators.required],
+      isActive : ['',Validators.required],
+      cityStateId : ['',Validators.required],
+      cityStateName : [''],
+      trainingCenterId : ['',Validators.required],
+      trainingCenterName : [''],
+      id : ['',Validators.required],
+      transactionUserId : ['',Validators.required]
+      // matautocompleteFieldName : [''],
+      });
+  
+      this.api.getAllCityState().subscribe((cityStates)=>{
+        this.cityStates = cityStates;
+      });
+      this.api.getAllTrainingCenter().subscribe((trainingCenteres)=>{
+        this.trainingCenteres = trainingCenteres;
+      });
+      this.hotkeysService.add(new Hotkey('ctrl+s', (event: KeyboardEvent): boolean => {
+        // Call the deleteGrade() function in the current component
+        this.addClassRoom();
+        return false; // Prevent the default browser behavior
+      }));
+  
+      if(this.editData){
+        this.actionBtn = "تعديل";
+        console.log("")
+      this.getModelData = this.editData;
+      this.modelForm.controls['transactionUserId'].setValue(this.editData.transactionUserId);
+        
+      this.modelForm.controls['code'].setValue(this.editData.code);
+      this.modelForm.controls['name'].setValue(this.editData.name);
+      this.modelForm.controls['address'].setValue(this.editData.address);
+      this.modelForm.controls['type'].setValue(this.editData.type);
+      this.modelForm.controls['capacity'].setValue(this.editData.capacity);
+      this.modelForm.controls['isActive'].setValue(this.editData.isActive);
+      this.modelForm.controls['cityStateId'].setValue(this.editData.cityStateId);
+      this.modelForm.controls['cityStateName'].setValue(this.editData.cityStateName);
+      this.modelForm.controls['trainingCenterId'].setValue(this.editData.trainingCenterId);
+      this.modelForm.controls['trainingCenterName'].setValue(this.editData.trainingCenterName);
+      // console.log("commodityId: ", this.modelForm.controls['commodityId'].value)
+      this.modelForm.addControl('id', new FormControl('', Validators.required));
+      this.modelForm.controls['id'].setValue(this.editData.id);
+      }
+    }
+
+    displayCourseName(course: any): string {
+      return course && course.name ? course.name : '';
+    }
+
+    cityStateSelected(event: MatAutocompleteSelectedEvent): void {
+      const cityState = event.option.value as course;
+      this.selectedCityState = cityState;
+      this.modelForm.patchValue({ cityStateId: cityState.id });
+      this.modelForm.patchValue({ cityStateName: cityState.name });
+    }
+
+    private _filterCityStates(value: string): course[] {
+      const filterValue = value.toLowerCase();
+      return this.cityStates.filter(cityState =>
+        cityState.name.toLowerCase().includes(filterValue) 
+      );
+    }
+
+    openAutoCourse() {
+      this.courseCtrl.setValue(''); // Clear the input field value
+    
+      // Open the autocomplete dropdown by triggering the value change event
+      this.courseCtrl.updateValueAndValidity();
+    }
+    
+    displayTrainingCenterName(trainingCenter: any): string {
+      return trainingCenter && trainingCenter.name ? trainingCenter.name : '';
+    }
+    trainingCenterSelected(event: MatAutocompleteSelectedEvent): void {
+      const trainingCenter = event.option.value as TrainingCenter;
+      this.selectedTrainingCenter = trainingCenter;
+      this.modelForm.patchValue({ trainingCenterId: trainingCenter.id });
+      this.modelForm.patchValue({ trainingCenterName: trainingCenter.name });
+    }
+
+    private _filterTrainingCenteres(value: string): TrainingCenter[] {
+      const filterValue = value.toLowerCase();
+      return this.trainingCenteres.filter(trainingCenter =>
+        trainingCenter.name.toLowerCase().includes(filterValue) 
+      );
+    }
+
+    openAutoTrainingCenter() {
+      this.trainingCenterCtrl.setValue(''); // Clear the input field value
+    
+      // Open the autocomplete dropdown by triggering the value change event
+      this.trainingCenterCtrl.updateValueAndValidity();
+    }
+    getExistingNames() {
+      this.api.getClassRoom().subscribe({
+        next: (res) => {
+          this.existingNames = res.map((item: any) => item.name);
+        },
+        error: (err) => {
+          console.log('Error fetching existing names:', err);
+        }
+      });
+    }
+
+  addClassRoom(){
+    if(!this.editData){
+      const enteredName = this.modelForm.get('name')?.value;
+
+    if (this.existingNames.includes(enteredName)) {
+      alert('هذا الاسم موجود من قبل، قم بتغييره');
+      return;
+    }
+      this.modelForm.removeControl('id')
+      // this.modelForm.controls['commodityId'].setValue(this.selectedOption.id);
+      this.modelForm.controls['transactionUserId'].setValue(this.transactionUserId);
+      console.log("add: ", this.modelForm.value);
+      if(this.modelForm.valid){
+        this.api.postClassRoom(this.modelForm.value)
+        .subscribe({
+          next:(res)=>{
+            this.toastrSuccess();
+            this.modelForm.reset();
+            this.dialogRef.close('save');
+          },
+          error:(err)=>{ 
+            this.toastrErrorSave();
+          }
+        })
+      }
+    }else{
+      this.updateClassRoom()
+    }
+  }
+
+  displayVendor (option:any):string {
+    return option && option.name ? option.name:'';
+
+  }
+      updateClassRoom(){
+        this.api.putClassRoom(this.modelForm.value)
+        .subscribe({
+          next:(res)=>{
+            this.toastrEdit();
+            this.modelForm.reset();
+            this.dialogRef.close('update');
+          },
+          error:()=>{
+            this.toastrErrorEdit();
+          }
+        })
+      }
+      toastrSuccess(): void {
+        this.toastr.success('تم الحفظ بنجاح');
+      }
+    
+      toastrEdit(): void {
+        this.toastr.success('تم التحديث بنجاح');
+      }
+    
+      toastrErrorSave(): void {
+        this.toastr.error('!خطأ عند حفظ البيانات');
+      }
+    
+      toastrErrorEdit(): void {
+        this.toastr.error('!خطأ عند تحديث البيانات');
+      }
+    
+  }
+  
+
+
+
+
+
--- src/app/modules/training/index/tr-budget/tr-budget.component.ts
+++ src/app/modules/training/index/tr-budget/tr-budget.component.ts
@@ -1,4 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+} from '@angular/material/dialog';
+
+import { ApiService } from '../../services/api.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { map, startWith } from 'rxjs/operators';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import { MatOptionSelectionChange } from '@angular/material/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { GlobalService } from 'src/app/pages/services/global.service'; 
+
+import { HotkeysService } from 'angular2-hotkeys';
+import { Hotkey } from 'angular2-hotkeys';
+import { ToastrService } from 'ngx-toastr';
+import { TrClassRoomDialogComponent } from '../tr-class-room-dialog/tr-class-room-dialog.component';
+import { TrBudgetDialogComponent } from '../tr-budget-dialog/tr-budget-dialog.component';
+
+
+export class courseName {
+  constructor(public id: number, public name: string,public global:GlobalService) {}
+}
+export class cityState {
+  constructor(public id: number, public name: string,public global:GlobalService) {}
+}
+export class trainingCenter {
+  constructor(public id: number, public name: string,public global:GlobalService) {}
+}
 
 @Component({
   selector: 'app-tr-budget',
@@ -6,5 +42,175 @@ import { Component } from '@angular/core';
   styleUrls: ['./tr-budget.component.css']
 })
 export class TrBudgetComponent {
+  cityStateCtrl: FormControl;
+  filteredCityStates: Observable<cityState[]>;
+  cityStates: cityState[] = [];
+  selectedCityState!: cityState;
+  trainingCenterCtrl: FormControl;
+  filteredTrainingCenteres: Observable<trainingCenter[]>;
+  trainingCenteres: trainingCenter[] = [];
+  selectedTrainingCenter!: trainingCenter;
+  formcontrol = new FormControl('');
+  gradeForm!: FormGroup;
+  title = 'Angular13Crud';
+  //define table fields which has to be same to api fields
+  displayedColumns: string[] = [ 'courseName','noTrainee','noHour','instructorHourFee','instructorTotalFee' ,'superVisingFee','otherFee','suppliesCost','transportCost','courseTotal', 'action'];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  
+  constructor(private dialog: MatDialog,private toastr: ToastrService, private api: ApiService,private global:GlobalService,private hotkeysService: HotkeysService) {
+    this.cityStateCtrl = new FormControl();
+    this.filteredCityStates = this.cityStateCtrl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterCityStates(value))
+    );
+    this.trainingCenterCtrl = new FormControl();
+    this.filteredTrainingCenteres = this.trainingCenterCtrl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filterTrainingCenteres(value))
+    );
+    global.getPermissionUserRoles('IT', '', 'الإدارة العامة للتدريب', 'supervised_user_circle')
+  }
+  ngOnInit(): void {
+    // console.log(productForm)
+    
+    this.getAllClassRooms();
+    this.api.getAllCityState().subscribe((cityStates) => {
+      this.cityStates = cityStates;
+    });
+    this.api.getAllTrainingCenter().subscribe((trainingCenteres) => {
+      this.trainingCenteres = trainingCenteres;
+    });
+    this.hotkeysService.add(new Hotkey('ctrl+o', (event: KeyboardEvent): boolean => {
+      // Call the deleteGrade() function in the current component
+      this.openDialog();
+      return false; // Prevent the default browser behavior
+    }));
+  }
+  openDialog() {
+    this.dialog
+      .open(TrBudgetDialogComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getAllClassRooms();
+        }
+      });
+  }
+
+  displayCityStateName(cityState: any): string {
+    return cityState && cityState.name ? cityState.name : '';
+  }
+  cityStateSelected(event: MatAutocompleteSelectedEvent): void {
+    const cityState = event.option.value as cityState;
+    this.selectedCityState = cityState;
+    this.gradeForm.patchValue({ cityStateId: cityState.id });
+    this.gradeForm.patchValue({ cityStateName: cityState.name });
+  }
+  private _filterCityStates(value: string): cityState[] {
+    const filterValue = value.toLowerCase();
+    return this.cityStates.filter(cityState =>
+      cityState.name.toLowerCase().includes(filterValue) 
+    );
+  }
+  displayTrainingCenterName(trainingCenter: any): string {
+    return trainingCenter && trainingCenter.name ? trainingCenter.name : '';
+  }
+  trainingCenterSelected(event: MatAutocompleteSelectedEvent): void {
+    const trainingCenter = event.option.value as trainingCenter;
+    this.selectedTrainingCenter = trainingCenter;
+    this.gradeForm.patchValue({ trainingCenterId: trainingCenter.id });
+    this.gradeForm.patchValue({ trainingCenterName: trainingCenter.name });
+  }
+  private _filterTrainingCenteres(value: string): trainingCenter[] {
+    const filterValue = value.toLowerCase();
+    return this.trainingCenteres.filter(trainingCenter =>
+      trainingCenter.name.toLowerCase().includes(filterValue) 
+    );
+  }
+  getAllClassRooms() {
+    this.api.getClassRoom().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert('Error');
+      },
+    });
+  }
+
+  editClassRoom(row: any) {
+    this.dialog
+      .open(TrClassRoomDialogComponent, {
+        width: '30%',
+        data: row,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getAllClassRooms();
+        }
+      });
+  }
+
+  deleteClassRoom(id: number) {
+    var result = confirm('هل ترغب بتاكيد الحذف ؟ ');
+    if (result) {
+      this.api.deleteClassRoom(id).subscribe({
+        next: (res) => {
+          if(res == 'Succeeded'){
+            console.log("res of deletestore:",res)
+            this.toastrDeleteSuccess();
+          this.getAllClassRooms();
+
+        }else{
+          alert(" لا يمكن الحذف لارتباطها بجداول اخري!")
+        }
+        },
+        error: () => {
+          alert('خطأ فى حذف العنصر');
+        },
+      });
+    }
+  }
+  
+
+  openAutoCityState() {
+    this.cityStateCtrl.setValue(''); // Clear the input field value
+  
+    // Open the autocomplete dropdown by triggering the value change event
+    this.cityStateCtrl.updateValueAndValidity();
+  }
+  openAutoTrainingCenter() {
+    this.trainingCenterCtrl.setValue(''); // Clear the input field value
+  
+    // Open the autocomplete dropdown by triggering the value change event
+    this.trainingCenterCtrl.updateValueAndValidity();
+  }
+ 
+
+  toastrDeleteSuccess(): void {
+    this.toastr.success('تم الحذف بنجاح');
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+
+
+
+
 
 }
