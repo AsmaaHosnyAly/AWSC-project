e3c0c366d67d1fbb82373782eca8a55e3039f6c4
Asmaa Hossni <A.Hossni@ISDT.local>
2023-11-12 11:49:08 +0200

    add page tilte in training screens

---
+++ src/app/modules/pro/index/pro-seller-type/pro-seller-type.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pro-seller-type',
+  templateUrl: './pro-seller-type.component.html',
+  styleUrls: ['./pro-seller-type.component.css']
+})
+export class ProSellerTypeComponent {
+
+}
